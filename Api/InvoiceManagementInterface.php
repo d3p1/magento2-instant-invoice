a6e7daf69044999e174ee85f1c469290231af9f5
C. M. de Picciotto <d3p1@d3p1.dev>
2026-03-27 10:36:58 -0300

    docs: improve phpDoc [#1]

--- Api/InvoiceManagementInterface.php
+++ Api/InvoiceManagementInterface.php
@@ -1,13 +1,9 @@
 <?php
 /**
- *
  * @description Invoice management interface
- *
- * @author Bina Commerce      <https://www.binacommerce.com>
- * @author C. M. de Picciotto <cmdepicciotto@binacommerce.com>
- *
- * @note Implementation to provide invoice utils that automate different invoice processes
- *
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ * @note        Implementation to provide invoice utils that
+ *              automate different invoice processes
  */
 namespace Bina\InstantInvoice\Api;
 
@@ -17,16 +13,14 @@ use Magento\Sales\Model\Order\Invoice;
 interface InvoiceManagementInterface
 {
     /**
-     *
      * Create invoice from order
      *
-     * @param Order $order
-     * @param bool  $sendEmail
-     *
+     * @param  Order $order
+     * @param  bool  $sendEmail
      * @return Invoice
-     *
-     * @note It is used the order and invoice model instead of their interface because it is required for the implementation logic
-     *
+     * @note   It is used the order and invoice model instead
+     *         of their interface because it is required
+     *         for the implementation logic
      */
     public function create(Order $order, $sendEmail = true);
 }
--- Model/InvoiceManagement.php
+++ Model/InvoiceManagement.php
@@ -1,13 +1,7 @@
 <?php
 /**
- *
  * @description Invoice management model
- *
- * @author Bina Commerce      <https://www.binacommerce.com>
- * @author C. M. de Picciotto <cmdepicciotto@binacommerce.com>
- *
- * @note Implementation to provide invoice utils that automate different invoice processes
- *
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 namespace Bina\InstantInvoice\Model;
 
@@ -21,141 +15,58 @@ use Bina\InstantInvoice\Api\InvoiceManagementInterface;
 class InvoiceManagement implements InvoiceManagementInterface
 {
     /**
-     *
      * @var InvoiceSender
-     *
      */
     protected $_invoiceSender;
 
     /**
-     *
      * @var TransactionFactory
-     *
      */
     protected $_transactionFactory;
 
     /**
-     *
      * Constructor
      *
      * @param InvoiceSender      $invoiceSender
      * @param TransactionFactory $transactionFactory
-     *
      */
     public function __construct(
         InvoiceSender      $invoiceSender,
         TransactionFactory $transactionFactory
     ) {
-        /**
-         *
-         * @note Init invoice sender
-         *
-         */
-        $this->_invoiceSender = $invoiceSender;
-
-        /**
-         *
-         * @note Init transaction factory
-         *
-         */
+        $this->_invoiceSender      = $invoiceSender;
         $this->_transactionFactory = $transactionFactory;
     }
 
     /**
-     *
-     * Create invoice from order
-     *
-     * @param Order $order
-     * @param bool  $sendEmail
-     *
-     * @return Invoice
-     *
-     * @note It is used the order and invoice model instead of their interface because it is required for the implementation logic
-     *
+     * {@inheritDoc}
      */
     public function create(Order $order, $sendEmail = true)
     {
-        /**
-         *
-         * @note Init transaction
-         *
-         */
         /** @var Transaction $transactionSave */
         $transactionSave = $this->_transactionFactory->create();
 
-        /**
-         *
-         * @note Prepare invoice
-         *
-         */
         $invoice = $order->prepareInvoice()->register();
-
-        /**
-         *
-         * @note It is necessary to set this flag to allow the platform to update order state (example: complete)
-         *
-         */
         $invoice->getOrder()->setIsInProcess(true);
-
-        /**
-         *
-         * @note Add objects to save
-         *
-         */
         $transactionSave->addObject($invoice)->addObject($invoice->getOrder());
-
-        /**
-         *
-         * @note Save
-         *
-         */
         $transactionSave->save();
 
-        /**
-         *
-         * @note Check if it is necessary to send email
-         *
-         */
         if ($sendEmail) {
-            /**
-             *
-             * @note Send invoice email
-             *
-             */
             $this->_sendInvoiceEmail($invoice);
         }
 
-        /**
-         *
-         * @note Return created invoice
-         *
-         */
         return $invoice;
     }
 
     /**
-     *
      * Send invoice email
      *
-     * @param Invoice $invoice
-     *
+     * @param  Invoice $invoice
      * @return void
-     *
      */
     protected function _sendInvoiceEmail(Invoice $invoice)
     {
-        /**
-         *
-         * @note Send invoice email
-         *
-         */
         $this->_invoiceSender->send($invoice);
-
-        /**
-         *
-         * @note Set customer notification flag
-         *
-         */
         $invoice->getOrder()->setCustomerNoteNotify(true);
     }
 }
